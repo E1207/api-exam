05deee0c41b427aed35db9e95af8b69de1c999d7
emmanuel <emmanuelpondi07@gmail.com>
2026-02-06 15:10:45 +0100

    feature : Middlewre auth configuration

--- src/tasks/dto/create-task.dto.ts
+++ src/tasks/dto/create-task.dto.ts
@@ -1,5 +1,13 @@
+import { IsString, IsNotEmpty, IsBoolean, IsDateString } from 'class-validator';
+
 export class CreateTaskDto {
+  @IsString()
+  @IsNotEmpty()
   title: string;
+
+  @IsDateString()
   startedAt: Date;
+
+  @IsBoolean()
   isCompleted: boolean;
 }
